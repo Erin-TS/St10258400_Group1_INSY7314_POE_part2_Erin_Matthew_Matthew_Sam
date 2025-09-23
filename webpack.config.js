0814db66646f98719d96d6fca09876378c0854c6
Erin Tyla Steenveld <st10258400@vcconnect.edu.za>
2025-09-23 13:03:38 +0200

    Add webpack configuration for project setup

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './Frontend/src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'Frontend/dist'),
+        filename: 'bundle.js',
+        publicPath: '/',
+        clean: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './Frontend/public/index.html'
+        })
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'Frontend/dist')
+        },
+        port:3000,
+        historyApiFallback: true,
+        hot: true
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    }
+};
