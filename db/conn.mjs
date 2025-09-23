e54f62837a0eeb49ca94fc31aeb18cb0228c7531
Sam Gusha <samanthagusha@icloud.com>
2025-09-23 11:13:02 +0200

    Created and connected the Mongodb database

---
+++ db/conn.mjs
@@ -0,0 +1,25 @@
+// This file is used to connect to the MongoDB database using the connection string stored in the .env file.
+
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
+
+const connectionString = process.env.ATLAS_URI || "";
+
+console.log(connectionString);
+
+const client = new MongoClient(connectionString);
+
+let conn;
+try {
+    conn = await client.connect();
+    console.log("Connected to MongoDB!!");
+} catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+}
+
+// Access database from connected client
+let db = client.db("INSY7314-Cluster");
+
+export default db;
+
