b16595abd3b20614b14ad126d9c8f438fab69b3b
Erin Tyla Steenveld <st10258400@vcconnect.edu.za>
2025-09-23 13:13:57 +0200

    created React entry point

---
+++ Frontend/src/index.js
@@ -0,0 +1,14 @@
+//this file is for the react enry point
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+// Get the root element from the HTML
+const container = document.getElementById('root');
+// Create a root.
+const root = createRoot(container);
+
+
+// Initial render
+root.render(<React.StrictMode><App /></React.StrictMode>);
