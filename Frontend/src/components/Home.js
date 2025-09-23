48a956615f1c42f8769bcc9eaf645c5f594f6446
Erin Tyla Steenveld <st10258400@vcconnect.edu.za>
2025-09-23 15:55:11 +0200

    Add Home component with navigation links for Customer and Employee portals

---
+++ Frontend/src/components/Home.js
@@ -0,0 +1,28 @@
+//this is the home page component
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './FormStyles.css';
+
+const Home = () => {
+    return (
+        <div className="home-container">
+            <h1>Welcome to the international payments portal!</h1>
+            <div classname="portal-sections">
+                <div className="Customer-section">
+                    <h2>Customer portal</h2>
+                    <div className="navigation-links">
+                        <Link to="/customer/login" className="nav-link">Login</Link>
+                        <Link to="/customer/register" className="nav-link">Register</Link>
+                    </div>
+                </div>
+                <div className="Employee-section">
+                    <h2>Employee portal</h2>
+                    <div className="navigation-links">
+                        <Link to="/employee/login" className="nav-link">Login</Link>
+                        </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+export default Home;
